64950799dc557f4d2acbb425b98618e0c15c45aa
Ricardo Ribeiro <ricardo.r.carvalho2001@gmail.com>
2026-06-22 23:23:31 -0300

    Adiciona cadastro, login e autenticação com JWT

---
+++ prisma/migrations/20260623015224_add_user/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "campus" TEXT,
+    "phone" TEXT,
+    "reputation" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
