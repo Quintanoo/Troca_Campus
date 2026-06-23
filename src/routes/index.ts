e54f34512eae71aa83f4ef0416df52dffc8f4a48
Ricardo Ribeiro <ricardo.r.carvalho2001@gmail.com>
2026-06-22 23:17:29 -0300

    wip: salva alterações iniciais do backend

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { categoryRoutes } from "./category.routes";
+import { authRoutes } from "./auth.routes";
 
 const routes = Router();
 
 routes.use("/categories", categoryRoutes);
+routes.use("/auth", authRoutes);
 
 export { routes };
